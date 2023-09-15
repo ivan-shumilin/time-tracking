51926af52b763196b8ccc5474433302deade4c0d
shimilin <shumilin.i.v@gmail.com>
2023-09-15 14:30:06 +0300

    fix: start project

--- swh.py
+++ swh.py
@@ -1,14 +1,10 @@
 import os
-
-
 import requests
-from dotenv import load_dotenv
 
 
-load_dotenv()
 TG_API = os.getenv('BOT_API_KEY')
 
-whook = '0c03e761f94319.lhr.life'
+whook = 'hr.petrushkagroup.ru'
 
 r = requests.get(f'https://api.telegram.org/bot{TG_API}/setWebhook?url=https://{whook}/')
 
